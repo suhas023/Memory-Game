729eba65bbc94ff15dc34b63c04389d5889d79a7
suhas023 <notsuhas@gmail.com>
2018-07-20 21:23:49 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -80,37 +80,37 @@ function main() {
 	let cards = [...deck.children];
 	let resetButtons = document.getElementsByClassName("restart");
 	[...resetButtons].forEach(button => button.addEventListener("click", ()=> {deck = init(cards, deck, container, performance);}))
-	deck = init(cards, deck, container, performance);		//initilize the game
+	deck = init(cards, deck, container, performance);                  //initilize the game
 }
 
 //resets the stats and adds event listener to the new deck
 function init(cards, deck, container, performance) {
-	let matchedListItems = [];	  //matched pairs stored here
-	let openListItem = [];			  //card, looking to be paired with immediate upcoming card
-	let bombList = [];						/*list of items that are soon to be hidden after mismatch,
+	let matchedListItems = [];        //matched pairs stored here
+	let openListItem = [];            //card, looking to be paired with immediate upcoming card
+	let bombList = [];                /*list of items that are soon to be hidden after mismatch,
 									 							events are blocked on them*/
 	deck = prepareNewDeck(cards, deck, container);//prepare new deck
 	performance.reset();
 	deck.addEventListener("click",
-		cardEvent.bind(null,openListItem, matchedListItems, bombList, performance));	//add event listener on the new deck for flipping cards
+		cardEvent.bind(null,openListItem, matchedListItems, bombList, performance));  //add event listener on the new deck for flipping cards
 	return deck;
 }
 
 //creats a new deck with shuffled cards
 function prepareNewDeck(cards, deck, container) {
-	let newDeck = deck.cloneNode(false);	//create a clone of the deck (false-shallow copy)
+	let newDeck = deck.cloneNode(false);    //create a clone of the deck (false-shallow copy)
 
-	hideCards(cards)											//hide cards before shuffle
-	cards = shuffle(cards);								//shuffle cards
-	cards.forEach(card => newDeck.appendChild(card));	//append shuffled cards to newDeck
-	container.replaceChild(newDeck, deck);						//replace old deck with newDeck
+	hideCards(cards);                       //hide cards before shuffle
+	cards = shuffle(cards);                 //shuffle cards
+	cards.forEach(card => newDeck.appendChild(card));  //append shuffled cards to newDeck
+	container.replaceChild(newDeck, deck);             //replace old deck with newDeck
 	return newDeck;
 }
 
 //function called when clicked on deck
 function cardEvent(openListItem, matchedListItems, bombList, performance, event) {
 	if(!bombList.includes(event.target)) {																	//check if the card is about to be closed(visually)
-		if(isListUniqueItem(event.target, openListItem, matchedListItems)) {	//check if the card is already selected or matched
+		if(isListUniqueItem(event.target, openListItem, matchedListItems)) {    //check if the card is already selected or matched
 			match(openListItem, event.target, openListItem, matchedListItems, bombList, performance);//if the card is unique call match
 		}
 	} else {
@@ -130,30 +130,30 @@ function isListUniqueItem(targetItem, openListItem, matchedListItems) {
 
 //displays the clicked card, tries to match the card clicked with previous clicked card
 function match(openListItem, targetItem, openListItem, matchedListItems, bombList, performance) {
-	let targetCard = targetItem.firstElementChild;	//get the target card
-	let targetSymbol = targetCard.classList[1];			//get the target symbol
-	let openItem = openListItem[0];									//get the previous un-matched opend card item if it exists
+	let targetCard = targetItem.firstElementChild;  //get the target card
+	let targetSymbol = targetCard.classList[1];     //get the target symbol
+	let openItem = openListItem[0];	                //get the previous un-matched opend card item if it exists
 	let openCard = null;
 
 	if(openListItem.length) {												//if a card to be paired is already selected, and target is 2nd in that pair
 		performance.incrementMoves();
-		openCard = openItem.firstElementChild;						//firstElementChild holds the card
-		if(openCard.classList.contains(targetSymbol)){		//if openCard contains same symbol as the target
-			matchedListItems.push(openItem, targetItem);		//push both to matched list
+		openCard = openItem.firstElementChild;                  //firstElementChild holds the card
+		if(openCard.classList.contains(targetSymbol)){          //if openCard contains same symbol as the target
+			matchedListItems.push(openItem, targetItem);    //push both to matched list
 			openListItem.shift();														//remove the first card as a pair is selected
 			show(targetItem);																//open the target card
-			if(matchedListItems.length === 16) {						//if all 16 cards are matched
+			if(matchedListItems.length === 16) {             //if all 16 cards are matched
 				performance.showModal();
 			}
 		} else {
 			show(targetItem);														//if they dont match, show the target card once. and  close it
 			openListItem.shift();												//both the cards in a pair are closed, so, openListItem is emptied for next pair
-			bombList.push(openItem, targetItem);				//push the pair to bombList indicating both are being closed and event on them is blocked
-			setTimeout(hideCards, 700, [openItem, targetItem], bombList);	//after 700ms close both the cards
+			bombList.push(openItem, targetItem); //push the pair to bombList indicating both are being closed and event on them is blocked
+			setTimeout(hideCards, 700, [openItem, targetItem], bombList);   //after 700ms close both the cards
 		}
 	} else {
-		openListItem.push(targetItem);	//if the target is firs one forming a pair push it to open list
-		show(targetItem);								//show the target item
+		openListItem.push(targetItem);  //if the target is firs one forming a pair push it to open list
+		show(targetItem);               //show the target item
 	}
 
 }
